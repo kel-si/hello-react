d69fa99a0169220656f23327659c7d85e4760557
Kelsi Olstad <kelsiolstad@gmail.com>
2022-03-21 20:51:11 -0600

    complete assignmnet

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { Children } from 'react/cjs/react.production.min';
 import './index.css';
 
 const Button = (props) => {
   // your code here
+  return (
+    <button>
+      {props.children}
+    </button>
+  );
 };
 
 const Application = () => {
-
+  const [name, setName] = useState("");
   // your code here
 
   const reset = () => {
+    setName("");
     console.log("reset");
     // your code here
   };
 
-  return (
-    <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
-    </main>
-  );
+    return (
+      <main>
+        <input value={name} onChange={(event) => setName(event.target.value)} placeholder="Type your name"></input>
+        <button onClick={reset}>reset</button>
+        <div>
+          {name
+          ? <h1>Hello {name}</h1>
+          : ""
+          }
+        </div>
+        
+      </main>
+    );
 };
 
 ReactDOM.render(<Application />, document.getElementById("root"));
